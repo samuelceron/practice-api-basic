6aecd09b6010823f4dee8adb33ebce1aab2d35b5
Samuel Cerón <samuelceron@MacBook-Pro-de-Samuel.local>
2022-06-06 10:20:50 -0500

    Adding item to favorites through POST

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-console.log('Hello world')
+const API_URL_RANDOM = `https://api.thecatapi.com/v1/images/search?limit=2&api_key=${API_KEY}`;
+const API_URL_FAVORITES = `https://api.thecatapi.com/v1/favourites/?api_key=${API_KEY}`;
 
-const API_URL = `https://api.thecatapi.com/v1/images/search?limit=3&api_key=${API_KEY}`;
-
+const spanError = document.getElementById('error');
 //Promesas
 // fetch(URL)
 //     .then(res => res.json())
@@ -11,20 +11,53 @@ const API_URL = `https://api.thecatapi.com/v1/images/search?limit=3&api_key=${AP
 //     })
 
 //ASYNC/AWAIT
-async function reload(){
-    const res = await fetch(API_URL)
+async function loadRandomCats(){
+    const res = await fetch(API_URL_RANDOM)
     const data = await res.json();
+    console.log("Random")
+    
     console.log(data)
-    const img1 = document.getElementById('img1');
-    const img2 = document.getElementById('img2');  
-    const img3 = document.getElementById('img3');  
+    
+    if (res.status !== 200) {
+        spanError.innerHTML = "Ocurrió un error: " + res.status + data.message;
+    } else{
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');  
 
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+
+        }
+    }
 
 
-
+async function loadFavoriteCats(){
+    const res = await fetch(API_URL_FAVORITES)
+    const data = await res.json();
+    console.log("Favorites")
+    console.log(data)
+    if (res.status !== 200) {
+        spanError.innerHTML = "Ocurrió un error: " + res.status + data.message;
+    }
 }
 
-reload();
+async function saveFavoriteMichis(){
+    const res = await fetch(API_URL_FAVORITES,{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            image_id: "MjAzMTMwNw"
+        }),
+    })
+
+    const data = await res.json();
+
+    if (res.status !== 200) {
+        spanError.innerHTML = "Ocurrió un error: " + res.status + data.message;
+    }
+}
+
+loadRandomCats();
+loadFavoriteCats();
